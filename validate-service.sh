bf6affe3f5d196e6145b9a73c194b734ab8de323
Kevin Dance <kdance@bottomline.com>
2021-07-13 16:00:25 +0100

    no message

--- validate-service.sh
+++ validate-service.sh
@@ -3,13 +3,14 @@ docker run  --rm \
 -e "PUBLISH_ARTIFACTS=true" \
 -e "PROJECT_NAME=capture-core-ocr" \
 -e "PROJECT_GROUP=com.bottomline.ptx.capture-core" \
--e "REPO_WITH_BINARIES_URL=http://host.docker.internal:8081/artifactory/libs-release-local" \
 -e "PROJECT_VERSION=0.0.1-SNAPSHOT" \
--e "EXTERNAL_CONTRACTS_GROUP_ID=com.bottomline.ptx.capture-core" \
+-e "REPO_WITH_BINARIES_URL=http://host.docker.internal:8081/artifactory/libs-release-local" \
 -e "EXTERNAL_CONTRACTS_ARTIFACT_ID=capture-core-ocr" \
+-e "EXTERNAL_CONTRACTS_GROUP_ID=com.bottomline.ptx.capture-core" \
 -e "EXTERNAL_CONTRACTS_VERSION=0.0.1-SNAPSHOT" \
 -e "EXTERNAL_CONTRACTS_REPO_WITH_BINARIES_URL=git://https://github.com/BottomlineDevTest/produce-contracts.git" \
 springcloud/spring-cloud-contract:2.2.0.BUILD-SNAPSHOT 
 
+echo ""
 echo "Waiting for 10 seconds"
 sleep 10
