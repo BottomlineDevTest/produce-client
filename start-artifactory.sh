0003347ef82efdaf06bbd2268754dc2b403ae3e4
Kevin Dance <kdance@bottomline.com>
2021-07-13 15:49:59 +0100

    no message

--- start-artifactory.sh
+++
@@ -1,8 +0,0 @@
-echo "Building docker"
-pushd docker
-yes | docker-compose rm -v
-docker-compose build
-docker-compose up -d
-popd
-echo "Waiting for 30 seconds for artifactory to boot properly"
-sleep 30
