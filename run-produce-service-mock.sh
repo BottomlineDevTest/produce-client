3ff2bce15a5061c461be4e4a3b97bd48298c0f1b
Kevin Dance <kdance@bottomline.com>
2021-07-13 15:22:36 +0100

    no message

--- run-produce-service-mock.sh
+++ run-produce-service-mock.sh
@@ -1,5 +1,4 @@
 docker run  --rm \
--e "APPLICATION_BASE_URL=http://host.docker.internal:5502" \
 -e "STUBRUNNER_IDS=com.ibm.developer:produce-service:+:stubs:9876" \
 -e "STUBRUNNER_REPOSITORY_ROOT=git://https://github.com/BottomlineDevTest/produce-contracts.git" \
 -e "STUBRUNNER_STUBS_MODE=REMOTE" \
--- validate-produce-service.sh
+++ validate-produce-service.sh
@@ -9,16 +9,7 @@ docker run  --rm \
 -e "EXTERNAL_CONTRACTS_ARTIFACT_ID=produce-service" \
 -e "EXTERNAL_CONTRACTS_VERSION=0.0.1-SNAPSHOT" \
 -e "EXTERNAL_CONTRACTS_REPO_WITH_BINARIES_URL=git://https://github.com/BottomlineDevTest/produce-contracts.git" \
--v "$(pwd)/build-output:/spring-cloud-contract-output/" \
--v "$(pwd)/contracts/:/contracts:ro" \
-springcloud/spring-cloud-contract:2.1.1.RELEASE
-
-#2.1.1.RELEASE
-#2.2.0.BUILD-SNAPSHOT --no tests
-#-e "EXTERNAL_CONTRACTS_PATH=/path/to/contracts/repo/root" \
-#-e "EXTERNAL_CONTRACTS_REPO_WITH_BINARIES_URL=git://https://github.com/BottomlineDevTest/produce-contracts.git" \
-#-e "EXTERNAL_CONTRACTS_PATH=/Users/William.Korando@ibm.com/code/collaborative-contract-driven-development-2-0/produce-contracts/" \
-#-e "APPLICATION_BASE_URL=http://host.docker.internal:8080" \
+springcloud/spring-cloud-contract:2.2.0.BUILD-SNAPSHOT 
 
 echo "Waiting for 10 seconds"
 sleep 10
