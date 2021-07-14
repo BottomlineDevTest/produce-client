070d834cc041ae745bd2b3696e4397f50464bc4b
Kevin Dance <kdance@bottomline.com>
2021-07-14 10:57:45 +0100

    no message

--- run-service-mock.sh
+++ run-service-mock.sh
@@ -1,6 +1,6 @@
 docker run  --rm \
 -e "STUBRUNNER_IDS=com.bottomline.ptx.capture-core:capture-core-ocr:+:stubs:9876" \
--e "STUBRUNNER_REPOSITORY_ROOT=git://https://github.com/BottomlineDevTest/produce-contracts.git" \
+-e "STUBRUNNER_REPOSITORY_ROOT=http://host.docker.internal:8081/artifactory/libs-release-local" \
 -e "STUBRUNNER_STUBS_MODE=REMOTE" \
 -p "8083:8083" \
 -p "9876:9876" \
